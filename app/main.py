c0a0bdaaad4056ac309ad537300e3a358649c573
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-26 16:44:21 +0200

    create and remove file

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class CleanUpFile:
     def __enter__(self) -> "CleanUpFile":
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(self, exc_type = None, exc_val = None, exc_tb = None) -> None:
         if os.path.exists(self.filename):
             try:
                 os.remove(self.filename)
